e93941b6edd4e23441e73c9ba017ecd8c577b42d
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2023-10-06 13:22:28 +0200

    .

---
+++ src/main/java/com/example/eindopdracht_fs_backend_matthijs_van_der_maas_3/security/ParticulierDetails.java
@@ -0,0 +1,62 @@
+package com.example.eindopdracht_fs_backend_matthijs_van_der_maas_3.security;
+
+        import com.example.eindopdracht_fs_backend_matthijs_van_der_maas_3.model.Particulier;
+        import com.example.eindopdracht_fs_backend_matthijs_van_der_maas_3.model.Role;
+        import org.springframework.security.core.GrantedAuthority;
+        import org.springframework.security.core.authority.SimpleGrantedAuthority;
+        import org.springframework.security.core.userdetails.UserDetails;
+
+        import java.util.Collection;
+        import java.util.HashSet;
+        import java.util.Set;
+
+public class ParticulierDetails implements Userdetails {
+
+    private final Particulier particulier;
+
+    public ParticulierDetails(Particulier particulier) {
+        this.particulier = particulier;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        for (Role role : particulier.getRoles()) {
+            authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return particulier.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return particulier.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // Implementeer logica voor accountverloop hier indien nodig
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // Implementeer logica voor vergrendeld account hier indien nodig
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // Implementeer logica voor vervallen verwijzingen hier indien nodig
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return Particulier.isEnabled();
+    }
+}
---
+++ src/main/java/com/example/eindopdracht_fs_backend_matthijs_van_der_maas_3/security/SecurityConfig.java
@@ -0,0 +1,80 @@
+package com.example.eindopdracht_fs_backend_matthijs_van_der_maas_3.security;
+
+import com.example.eindopdracht_fs_backend_matthijs_van_der_maas_3.repository.ParticulierRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.NotActiveException;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtService jwtService;
+    private final ParticulierRepository particulierRepository;
+
+    public SecurityConfig(JwtService jwtService, ParticulierRepository particulierRepository) {
+        this.jwtService = jwtService;
+        this.particulierRepository = particulierRepository;
+    }
+
+    @Bean
+    public ParticulierDetailsService particulierDetailsService() {
+        return new ParticulierDetailsService(particulierRepository);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtRequestFilter jwtRequestFilter() {
+        return new JwtRequestFilter(jwtService, particulierDetailsService());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws NotActiveException {
+        http
+                .antMatchers("/", "/login_page", "/productie_informatie", "/inschrijfformulier",
+                        "/inschrijfformulier_particulier", "/inschrijfformulier_producent").permitAll()
+                .antMatchers("/mijn_bieren", "/mijn_pagina").hasAnyRole("USER", "ADMIN", "BREWER")
+                .antMatchers("/inschrijfformulier_product").hasRole("BREWER")
+
+                .antMatchers(HttpMethod.POST, "/inschrijfformulier_particulier" ).hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PUT, "/inschrijfformulier_particulier" ).hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/inschrijfformulier_particulier" ).hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/inschrijfformulier_particulier").hasAnyRole("ADMIN")
+
+                .antMatchers(HttpMethod.POST, "/inschrijfformulier_producent" ).hasAnyRole("BREWER", "ADMIN")
+                .antMatchers(HttpMethod.PUT, "/inschrijfformulier_producent" ).hasAnyRole("BREWER", "ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/inschrijfformulier_producent" ).hasAnyRole("BREWER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/inschrijfformulier_producent").hasAnyRole("ADMIN")
+
+                .antMatchers(HttpMethod.POST, "/inschrijfformulier_product" ).hasAnyRole("BREWER", "ADMIN")
+                .antMatchers(HttpMethod.PUT, "/inschrijfformulier_product" ).hasAnyRole("BREWER", "ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/inschrijfformulier_product" ).hasAnyRole("BREWER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/inschrijfformulier_product").hasAnyRole("ADMIN")
+                .anyRequest().denyAll()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf()
+                .disable()
+                .addFilterBefore(jwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+}
