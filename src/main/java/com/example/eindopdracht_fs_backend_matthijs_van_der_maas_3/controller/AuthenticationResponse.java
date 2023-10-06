e6c06b68c3162b5e5cc2bf4ae937f7df93309c15
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2023-10-06 13:22:28 +0200

    .

---
+++ src/main/java/com/example/eindopdracht_fs_backend_matthijs_van_der_maas_3/controller/AuthenticationResponse.java
@@ -0,0 +1,6 @@
+package com.example.eindopdracht_fs_backend_matthijs_van_der_maas_3.controller;
+
+public class AuthenticationResponse {
+    public AuthenticationResponse(String jwt) {
+    }
+}
---
+++ src/main/java/com/example/eindopdracht_fs_backend_matthijs_van_der_maas_3/dto/ParticulierDto.java
@@ -0,0 +1,69 @@
+package com.example.eindopdracht_fs_backend_matthijs_van_der_maas_3.dto;
+
+import com.example.eindopdracht_fs_backend_matthijs_van_der_maas_3.model.Role;
+import jakarta.validation.constraints.Email;
+
+
+import java.util.List;
+
+public class ParticulierDto {
+
+
+    public String firstName;
+
+    public String lastName;
+
+
+    @Email
+    public String email;
+
+
+    public String userName;
+
+
+    public String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return getRoles();
+    }
+}
