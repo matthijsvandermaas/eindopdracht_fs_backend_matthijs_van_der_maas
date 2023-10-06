d20f37bfdbd8ac1f21c0817f08448cd65c8f9e54
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2023-10-06 13:22:28 +0200

    .

---
+++ src/main/java/com/example/eindopdracht_fs_backend_matthijs_van_der_maas_3/service/CustomUserDetailsService.java
@@ -0,0 +1,81 @@
+package com.example.eindopdracht_fs_backend_matthijs_van_der_maas_3.service;
+
+
+import com.example.eindopdracht_fs_backend_matthijs_van_der_maas_3.dto.UserDto;
+import com.example.eindopdracht_fs_backend_matthijs_van_der_maas_3.model.Authority;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+//    @Autowired
+//    private AuthorityService authorityService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        UserDto userDto = userService.getUser(username);
+
+
+        String password = userDto.getPassword();
+
+        Set<Authority> authorities = userDto.getAuthorities();
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for (Authority authority: authorities) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.getAuthority()));
+        }
+
+        return new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return getAuthorities();
+            }
+
+            @Override
+            public String getPassword() {
+                return getPassword();
+            }
+
+            @Override
+            public String getUsername() {
+                return getUsername();
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return false;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return false;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return false;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return false;
+            }
+        };
+    }
+
+}
