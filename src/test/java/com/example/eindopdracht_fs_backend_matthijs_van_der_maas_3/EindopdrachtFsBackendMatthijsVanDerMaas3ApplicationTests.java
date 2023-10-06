bf493e5cdee350727a4cf0d09cc7e2a11e4ad1b4
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2023-10-06 13:22:28 +0200

    .

---
+++ src/test/java/com/example/eindopdracht_fs_backend_matthijs_van_der_maas_3/EindopdrachtFsBackendMatthijsVanDerMaas3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eindopdracht_fs_backend_matthijs_van_der_maas_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtFsBackendMatthijsVanDerMaas3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
